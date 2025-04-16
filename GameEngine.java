847ba7dfeeef18642027d20e12a5be90d99275fe
miku3223 <abnergeng@gmail.com>
2025-04-16 13:04:01 -0700

    part 2 finished, uncompleted part 3
    Print to test part 2

---
+++ GameEngine.java
@@ -0,0 +1,9 @@
+public class GameEngine {
+    public static void main(String[] args) {
+        SudokuBoard sudoku = new SudokuBoard("data1.sdk");
+
+        System.out.println(sudoku);
+
+    }
+
+}
--- SudokuBoard.java
+++ SudokuBoard.java
@@ -1,11 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class SudokuBoard {
-    private char[][] board;
+    private char[][] board = new char[9][9];
 
     public SudokuBoard(String fileName){
-        
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            int r = 0;
+
+            while (scanner.hasNextLine() && r < 9) {
+                String line = scanner.nextLine();
+                for (int c = 0; c < 9; c++) {
+                    board[r][c] = line.charAt(c);
+                }
+                r++;
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot load: " + fileName);
+        } catch (InputMismatchException e) {
+            System.out.println(fileName + " does not meet format expectations.");
+        }
     }
 
-    public String toString(){
+
+    public String toString(char[][] board){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
         return "";
     }
 }
